1609a59873049850736226406aaf4fc9e9f0b20d
dhanushkumar <dhanushkumaramk@gmail.com>
2026-04-25 11:40:37 +0530

    feat: implement FastAPI endpoints for retrieving employee data via path and query parameters

--- AI ENGINEERING/fastApi/main.py
+++ AI ENGINEERING/fastApi/main.py
@@ -1,5 +1,20 @@
-from fastapi import FastApi
+from fastapi import FastAPI
 
-app = FastApi()
+app = FastAPI()
 
-@app.get
+emp = [
+    {'name' : "dhanushlumar", "id" : 1, "place": "USA"},
+    {"name" : "sanjay", "id" : 2, "place" : "Canada"}
+]
+
+@app.get("/display/{id}")
+def viewforPath(id : int):
+    for e in emp:
+        if e['id'] == id:
+            return e
+
+@app.get("/display")
+def viewForQuery(id : int):
+    for e in emp:
+        if e['id']==id:
+            return e

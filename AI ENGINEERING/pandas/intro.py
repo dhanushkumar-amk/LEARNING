79124bcc729c21cfc9c6cf18eb3e64cfa36e3084
dhanushkumar <dhanushkumaramk@gmail.com>
2026-04-30 09:17:22 +0530

    modified

--- AI ENGINEERING/pandas/intro.py
+++ AI ENGINEERING/pandas/intro.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
-print(pd.__version__)
+# pandas = panel data
+# focus on two concepts series and data frames
 
+# series => 1D array
+# data frames => table like structure
+
+# pandas store , maupulate, import and export like stuffs
+
+
+print(pd.__version__)
